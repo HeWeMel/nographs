105afc794a10fb34a1162462e62b2fb08800bfd2
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2022-03-28 22:26:00 +0200

    Status corrected

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     package_dir={'': 'src'},
     # install_requires=[],
     classifiers=[
-        'Development Status :: 5 - Stable',
+        'Development Status :: 5 - Production/Stable',
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.9",
