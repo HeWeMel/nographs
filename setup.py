f08300a3bed56067b76d5f750228b15953703ddd
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2023-02-09 21:18:02 +0100

    Version changed to v3.0.3

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.rst').read_text(encoding='utf-8')
 
 setup(
     name="nographs",
-    version="3.0.2",  # prev dev: 3.0.0.dev2
+    version="3.0.3",  # prev dev: 3.0.0.dev2
     description=("Graph analysis – the lazy (evaluation) way: Analysis on the fly, "
                  + "for graphs, that are computed and/or adapted on the fly."),
     long_description=long_description,
