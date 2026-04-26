8394ea4af3e0591fa07460c50bf35649786a29a2
HeWeMel <69761336+HeWeMel@users.noreply.github.com>
2026-04-26 20:54:32 +0200

    Mypy: Removed temporary "type: ignore" that I had to use due to a bug in mypy 1.20 in the analysis of a check for equality of two generic functions (https://github.com/python/mypy/issues/21182). This bug is solved now in 1.20.2.

--- src/nographs/_strategies/traversals/traversal.py
+++ src/nographs/_strategies/traversals/traversal.py
@@ -212,11 +212,7 @@ class Traversal(Strategy[T_vertex, T_vertex_id, T_labels], Iterable[T_vertex]):
         def my_generator() -> Iterator[T_vertex]:
             vertex_to_id = self._vertex_to_id
             if vertex_to_id == vertex_as_id:
-                vertex_set = set(cast(Iterable[T_vertex_id], vertices))  # type: ignore[unreachable]
-                # The type ignore above is temporary, as a workaround for
-                # the following issue of mypy 1.20:
-                # https://github.com/python/mypy/issues/21182
-
+                vertex_set = set(cast(Iterable[T_vertex_id], vertices))
                 v_count = len(vertex_set)
                 if v_count:
                     for v in self._generator:
@@ -278,10 +274,7 @@ class Traversal(Strategy[T_vertex, T_vertex_id, T_labels], Iterable[T_vertex]):
 
         vertex_to_id = self._vertex_to_id
         if vertex_to_id == vertex_as_id:
-            for v in self._generator:  # type: ignore[unreachable]
-                # The type ignore above is temporary, as a workaround for
-                # the following issue of mypy 1.20:
-                # https://github.com/python/mypy/issues/21182
+            for v in self._generator:
                 if v != vertex:
                     continue
                 return v
